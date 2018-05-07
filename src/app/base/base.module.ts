8da35ae69f8bce96ccdebb25fe6df7426d5175d2
bfv <dev@bfv.io>
2018-05-07 17:27:21 +0200

    added main-page

--- src/app/base/base.module.ts
+++ src/app/base/base.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ThirdpartyModule } from './thirdparty/thirdparty.module';
+import { MainPageComponent } from './main-page/main-page.component';
 
 @NgModule({
   imports: [
     CommonModule,
     ThirdpartyModule,
   ],
-  declarations: [],
+  declarations: [MainPageComponent],
   exports: [
-    ThirdpartyModule
+    ThirdpartyModule,
+    MainPageComponent,
   ]
 })
 export class BaseModule { }
